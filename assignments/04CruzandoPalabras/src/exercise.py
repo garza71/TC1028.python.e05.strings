2c5db3fddae33ef121d2fb29dca71072753af722
Lina Patricia Garza Gomez <57541431+garza71@users.noreply.github.com>
2021-09-13 18:17:16 -0500

    Update exercise.py

--- assignments/04CruzandoPalabras/src/exercise.py
+++ assignments/04CruzandoPalabras/src/exercise.py
@@ -1,12 +1,9 @@
+import math
 def main():
     #escribe tu código abajo de esta línea
-    import math
-    a=input()
-    b=input()
-    ma=math.ceil(len(a)/2)
-    mb=math.ceil(len(b)/2)
-    print(a[0:ma]+b[mb::])
-    print(a[ma::]+b[0:mb])
+
+    #a=input()
+    #ma=math.ceil(len(a)/2)
 
 if __name__=='__main__':
     main()
